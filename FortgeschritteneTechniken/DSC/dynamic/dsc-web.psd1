c406b3b3c1790e833d4c73d61a4ed3624934f57d
Stefan Ober <StefanO@ppedv.de>
2021-12-15 11:51:15 +0100

    Demo: DSC

---
+++ FortgeschritteneTechniken/DSC/dynamic/dsc-web.psd1
@@ -0,0 +1,17 @@
+﻿@{
+    AllNodes = @(
+        @{
+            NodeNAme = "Member1"
+            WindowsFeatures = @(
+            @{
+                Name="Web-Server"
+                Ensure = "Present"
+            },
+            @{
+                Name = "Web-Mgmt-tools"
+                ensure = "Present"
+            }
+        )
+        }
+    )
+}
---
+++ FortgeschritteneTechniken/DSC/lcm/lcm.ps1
@@ -0,0 +1,35 @@
+﻿configuration SimpleWebServer
+{
+    # Zum Abrufen der Knotenliste können Ausdrücke ausgewertet werden.
+    # Beispiel: $AllNodes.Where("Role -eq Web").NodeName
+    node ("Member1")
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = "ApplyAndAutoCorrect"
+            ConfigurationModeFrequencyMins = 15
+        }
+
+        # Ressourcenanbieter aufrufen
+        # Beispiel: WindowsFeature, File
+        WindowsFeature WebServerRole
+        {
+           Ensure = "Present"
+           Name   = "Web-Server"
+        }
+
+        WindowsFeature WebVerwaltung
+        {
+           Ensure = "Present"
+           Name   = "Web-Mgmt-Tools"
+           DependsOn = "[WindowsFeature]WebServerRole"
+        }
+      
+    }
+}
+
+#zum erzeugen der MOF Datei
+SimpleWebServer -OutputPath "C:\KursFiles\FortgeschritteneTechniken\DSC\lcm"
+
+#zum Anwenden der Konfiguration auf die angegeben Nodes
+#Start-DscConfiguration -Wait -Verbose -Path "C:\KursFiles\FortgeschritteneTechniken\DSC\lcm"
